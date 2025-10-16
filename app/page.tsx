1a4c11fa40ca711bd57e3ebd1bdc1752ed6cde95
dieeegue <dieeegue2104@gmail.com>
2025-10-16 22:29:28 +0200

    refactor: Arregla alineación del título y mejora navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Header } from '@/components/ui/header';
 import { HighlightedText } from '@/components/ui/highlightedText';
-import { Input } from '@/components/ui/input';
 import Marquee from '@/components/ui/marquee';
 import { Navbar } from '@/components/ui/navbar';
 import StarWithStroke from '@/components/ui/starWithStroke';
@@ -11,7 +9,7 @@ import { Github } from 'lucide-react';
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="sticky top-0 z-20">
         <Navbar>
           <Button variant="neutral" size="icon">
             <Github />
@@ -19,7 +17,7 @@ export default function Home() {
         </Navbar>
       </header>
       <main>
-        <div className="flex flex-col gap-4 h-[85dvh] justify-center items-center bg-[linear-gradient(to_right,#8080804D_1px,transparent_1px),linear-gradient(to_bottom,#80808090_1px,transparent_1px)] bg-[size:70px_70px]">
+        <div className="flex flex-col gap-4 min-h-[75dvh] justify-center items-center bg-[linear-gradient(to_right,#8080804D_1px,transparent_1px),linear-gradient(to_bottom,#80808090_1px,transparent_1px)] bg-[size:70px_70px] py-5">
           <div className="flex flex-col justify-center max-w-[66dvw]">
             <h1 className="text-center text-3xl sm:text-4xl md:text-5xl font-bold mb-4 md:mb-6 p-2">
               Empieza a <HighlightedText>acortar</HighlightedText> los enlaces
@@ -55,6 +53,7 @@ export default function Home() {
           ]}
         />
       </main>
+      <div className="min-h-[100dvh]"></div>
     </>
   );
 }
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar({ ...props }: React.ComponentProps<'nav'>) {
   return (
     <nav
-      className="fixed left-0 top-0 z-20 mx-auto flex h-[70px] w-full items-center border-b-4 border-border bg-secondary-background px-5 justify-end"
+      className="mx-auto flex min-h-[10dvh] w-full items-center border-b-4 border-border bg-secondary-background px-5 justify-end"
       {...props}
     />
   );
