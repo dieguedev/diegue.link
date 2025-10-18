a0285e950cb6dc7a227acfcb0c7d6ba44b5b80dc
dieeegue <dieeegue2104@gmail.com>
2025-10-18 22:36:11 +0200

    chore: Incorpora Google Analytics al proyecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { DM_Sans, Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-B4ES60755M" />
     </html>
   );
 }
