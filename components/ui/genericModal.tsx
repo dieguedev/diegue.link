14bdc346ff6f6b42d7badda4bf8071e1f508a774
dieeegue <dieeegue2104@gmail.com>
2025-12-06 19:12:51 +0100

    refactor: Añade custom-div para que se muestre el backdrop con modal siendo false

--- components/ui/genericModal.tsx
+++ components/ui/genericModal.tsx
@@ -9,14 +9,22 @@ interface GenericModalProps {
 }
 
 export function GenericModal({ title, trigger, children }: GenericModalProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
-    <DialogPrimitive.Root modal={false}>
+    <DialogPrimitive.Root open={isOpen} onOpenChange={setIsOpen} modal={false}>
       <DialogPrimitive.Trigger asChild>{trigger}</DialogPrimitive.Trigger>
 
       <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay className="fixed inset-0 bg-black/50 z-40" />
+        {isOpen && (
+          <div 
+            className="fixed inset-0 bg-black/50 z-40 animate-in fade-in-0 duration-200" 
+            onClick={() => setIsOpen(false)}
+            aria-hidden="true"
+          />
+        )}
 
-        <DialogPrimitive.Content className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 outline-none">
+        <DialogPrimitive.Content className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 outline-none animate-in fade-in-0 zoom-in-95 duration-200">
           <VisuallyHidden>
             <DialogPrimitive.Title>{title}</DialogPrimitive.Title>
           </VisuallyHidden>
