64ac1d1d3f18d32d45056b94e6ff79f981390484
Diego Bogo <91671521+dieguedev@users.noreply.github.com>
2026-03-27 15:03:08 +0100

    refactor: Cachea la llamada a get-stargazers durante 1 hora para evitar llegar al limit rate (#28)

--- domain/services/get-stargazers.ts
+++ domain/services/get-stargazers.ts
@@ -1,9 +1,14 @@
 import { githubClient } from '@/lib/github/github-client';
+import { unstable_cache } from 'next/cache';
 
-export async function getStargazers() {
-  const { data } = await githubClient.request('GET /repos/{owner}/{repo}', {
-    owner: 'dieguedev',
-    repo: 'diegue.link',
-  });
-  return data.stargazers_count;
-}
+export const getStargazers = unstable_cache(
+  async () => {
+    const { data } = await githubClient.request('GET /repos/{owner}/{repo}', {
+      owner: 'dieguedev',
+      repo: 'diegue.link',
+    });
+    return data.stargazers_count;
+  },
+  ['github-stars'],
+  { revalidate: 3600 },
+);
