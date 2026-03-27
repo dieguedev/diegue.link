08df704810f237fa43098560134a699d1ba8dac0
dieeegue <dieeegue2104@gmail.com>
2026-03-27 12:47:27 +0100

    bug: Protege la ruta de Dashboard para que sólo puedan acceder usuarios autenticados

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import { Suspense } from 'react';
+import { auth } from '@/lib/auth/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { UrlForm } from './url-form';
 
 export default async function Dashboard() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) redirect('/');
+
   return (
     <div className="container mx-auto py-10">
       <Suspense fallback={<div className="text-center">Cargando...</div>}>
